e20180c9b7246622b64b6447d3d8887c80eb1f86
powhound4 <george.hatch@fathym.com>
2019-05-07 14:42:44 -0600

    test routing works

--- projects/demo/src/app/app-routing.module.ts
+++ projects/demo/src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TestPageComponent } from '@lowcodeunit/lcu-hamburger-menu-common';
 
 const routes: Routes = [
-{ path: '', redirectTo: 'home', pathMatch: 'full'},
+//{ path: '', redirectTo: 'home', pathMatch: 'full'},
+{ path: 'home', component: TestPageComponent},
 ];
 
 @NgModule({
