041a4a008a6f81792fc3444612a2cbea25d66f78
powhound4 <george.hatch@fathym.com>
2019-05-08 11:57:04 -0600

    change material module

--- projects/common/src/lib/controls/hamburger-menu/hamburger-menu.component.ts
+++ projects/common/src/lib/controls/hamburger-menu/hamburger-menu.component.ts
@@ -2,16 +2,15 @@ import { Component, OnInit, Input } from '@angular/core';
 import { HamburgerMenuModel } from '../../models/hamburger-menu.model';
 
 
+
 @Component({
   selector: 'lcu-hamburger-menu',
   templateUrl: './hamburger-menu.component.html',
   styleUrls: ['./hamburger-menu.component.scss']
 })
 export class HamburgerMenuComponent implements OnInit {
-  // public MenuItems: Array<HamburgerMenuModel>;
+  
   @Input() MenuItems: Array<HamburgerMenuModel>;
-  //protected _menuItems: any;
   ngOnInit() {
-  //   this.MenuItems = Constants.VAR_NAMES_MENU;
   }
 }
